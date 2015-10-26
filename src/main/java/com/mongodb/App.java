53c7b9137a74dd280daea5569df57e7e2c402190
gustavo pinheiro <gustavo@gusdubuntuvm.gushome.net>
2015-10-26 00:26:17 -0500

    testing java drive

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -1,13 +1,23 @@
 package com.mongodb;
 
-/**
- * Hello world!
- *
- */
+import org.bson.Document;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
+        MongoClient client = new MongoClient(new ServerAddress(), options );
+        
+        MongoDatabase db = client.getDatabase("m101");
+        
+        MongoCollection<Document> coll = db.getCollection("hw1");
+        
+        coll.find();
+        
+        client.close();
     }
 }
